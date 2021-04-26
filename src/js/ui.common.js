218fa74b19c8461bb7b91b83a236c0264f88ffb1
asever77 <johyunmin77@gmail.com>
2021-04-26 17:53:30 +0900

    헤헤

--- src/js/ui.common.js
+++ src/js/ui.common.js
@@ -5,151 +5,151 @@
 	netive.common = {
 		init : () => {
 
-			var fristHref = '/netiveUI/html/start/introduction.html';
+			var fristHref = '/public/html/start/introduction.html';
 			
 			if (!!netive.uiParts.para('page')) {
 				switch(netive.uiParts.para('page')) {
 					case 'introduction' :
-						fristHref = '/netiveUI/html/start/introduction.html';
+						fristHref = '/public/html/start/introduction.html';
 						break;
 					case 'typography' :
-						fristHref = '/netiveUI/html/start/typography.html';
+						fristHref = '/public/html/start/typography.html';
 						break;
 					case 'device' :
-						fristHref = '/netiveUI/html/start/device.html';
+						fristHref = '/public/html/start/device.html';
 						break;
 					case 'margin' :
-						fristHref = '/netiveUI/html/start/margin.html';
+						fristHref = '/public/html/start/margin.html';
 						break;
 					case 'naming' :
-						fristHref = '/netiveUI/html/start/naming.html';
+						fristHref = '/public/html/start/naming.html';
 						break;
 					case 'placeholder' :
-						fristHref = '/netiveUI/html/start/placeholder.html';
+						fristHref = '/public/html/start/placeholder.html';
 						break;
 					case 'units' :
-						fristHref = '/netiveUI/html/start/units.html';
+						fristHref = '/public/html/start/units.html';
 						break;
 
 					case 'bulletList' :
-						fristHref = '/netiveUI/html/contents/bulletList.html';
+						fristHref = '/public/html/contents/bulletList.html';
 						break;
 					case 'table' :
-						fristHref = '/netiveUI/html/contents/table.html';
+						fristHref = '/public/html/contents/table.html';
 						break;
 					case 'inputFormat' :
-						fristHref = '/netiveUI/html/contents/inputFormat.html';
+						fristHref = '/public/html/contents/inputFormat.html';
 						break;
 					case 'inputPlaceholder' :
-						fristHref = '/netiveUI/html/components/inputPlaceholder.html';
+						fristHref = '/public/html/components/inputPlaceholder.html';
 						break;
 
 
 					case 'accordion' :
-						fristHref = '/netiveUI/html/components/accordion.html';
+						fristHref = '/public/html/components/accordion.html';
 						break;
 					case 'brickList' :
-						fristHref = '/netiveUI/html/components/brickList.html';
+						fristHref = '/public/html/components/brickList.html';
 						break;
 					case 'draggable' :
 						console.log('draggable');
-						fristHref = '/netiveUI/html/components/draggable.html';
+						fristHref = '/public/html/components/draggable.html';
 						break;
 					case 'dropdown' :
-						fristHref = '/netiveUI/html/components/dropdown.html';
+						fristHref = '/public/html/components/dropdown.html';
 						break;
 					case 'floating' :
-						fristHref = '/netiveUI/html/components/floating.html';
+						fristHref = '/public/html/components/floating.html';
 						break;
 					case 'floatingRange' :
-						fristHref = '/netiveUI/html/components/floatingRange.html';
+						fristHref = '/public/html/components/floatingRange.html';
 						break;
 					case 'modal' :
-						fristHref = '/netiveUI/html/components/modal.html';
+						fristHref = '/public/html/components/modal.html';
 						break;
 					case 'scrollBar' :
-						fristHref = '/netiveUI/html/components/scrollBar.html';
+						fristHref = '/public/html/components/scrollBar.html';
 						break;
 					case 'parallax' :
-						fristHref = '/netiveUI/html/components/parallax.html';
+						fristHref = '/public/html/components/parallax.html';
 						break;
 					case 'popupBook' :
-						fristHref = '/netiveUI/html/components/popupBook.html';
+						fristHref = '/public/html/components/popupBook.html';
 						break;
 					case 'loading' :
-						fristHref = '/netiveUI/html/components/loading.html';
+						fristHref = '/public/html/components/loading.html';
 						break;
 					case 'tab' :
-						fristHref = '/netiveUI/html/components/tab.html';
+						fristHref = '/public/html/components/tab.html';
 						break;
 					case 'tableCaption' :
-						fristHref = '/netiveUI/html/components/tableCaption.html';
+						fristHref = '/public/html/components/tableCaption.html';
 						break;
 					case 'tableCellFix' :
-						fristHref = '/netiveUI/html/components/tableCellFix.html';
+						fristHref = '/public/html/components/tableCellFix.html';
 						break;
 					case 'tableScroll' :
-						fristHref = '/netiveUI/html/components/tableScroll.html';
+						fristHref = '/public/html/components/tableScroll.html';
 						break;
 					case 'print' :
-						fristHref = '/netiveUI/html/components/print.html';
+						fristHref = '/public/html/components/print.html';
 						break;
 					case 'popup' :
-						fristHref = '/netiveUI/html/components/popup.html';
+						fristHref = '/public/html/components/popup.html';
 						break;
 					case 'tooltip' :
-						fristHref = '/netiveUI/html/components/tooltip.html';
+						fristHref = '/public/html/components/tooltip.html';
 						break;
 					case 'datePicker' :
-						fristHref = '/netiveUI/html/components/datePicker2.html';
+						fristHref = '/public/html/components/datePicker2.html';
 						break;
 					case 'inputClear' :
-						fristHref = '/netiveUI/html/components/inputClear.html';
+						fristHref = '/public/html/components/inputClear.html';
 						break;
 					case 'select' :
-						fristHref = '/netiveUI/html/components/select.html';
+						fristHref = '/public/html/components/select.html';
 						break;
 					case 'innerLabel' :
-						fristHref = '/netiveUI/html/components/innerLabel.html';
+						fristHref = '/public/html/components/innerLabel.html';
 						break;
 					case 'scrollMove' :
-						fristHref = '/netiveUI/html/components/scrollMove.html';
+						fristHref = '/public/html/components/scrollMove.html';
 						break;
 					case 'countNumber' :
-						fristHref = '/netiveUI/html/components/countNumber.html';
+						fristHref = '/public/html/components/countNumber.html';
 						break;
 
 					case 'layout' :
-						fristHref = '/netiveUI/html/contents/layout.html';
+						fristHref = '/public/html/contents/layout.html';
 						break;
 					case 'button' :
-						fristHref = '/netiveUI/html/contents/button.html';
+						fristHref = '/public/html/contents/button.html';
 						break;
 					case 'jsonCodingList' :
-						fristHref = '/netiveUI/html/components/jsonCodingList.html';
+						fristHref = '/public/html/components/jsonCodingList.html';
 						break;
 					case 'fileUpload' :
-						fristHref = '/netiveUI/html/components/fileUpload.html';
+						fristHref = '/public/html/components/fileUpload.html';
 						break;
 					case 'slider' :
-						fristHref = '/netiveUI/html/components/slider.html';
+						fristHref = '/public/html/components/slider.html';
 						break;
 					case 'issue' :
-						fristHref = '/netiveUI/html/memory/issue.html';
+						fristHref = '/public/html/memory/issue.html';
 						break;
 				   
 				}
 			} 
 
 			netive.uiAjax.request({ 
-				src: '/netiveUI/html/inc/header.html', 
+				src: '/public/html/inc/header.html', 
 				callback: function(v){
 					netive.common.header(v);
 				} 
 			});
 	
 			netive.uiAjax.request({ 
-				src: '/netiveUI/html/inc/footer.html', 
+				src: '/public/html/inc/footer.html', 
 				callback: function(v){
 					netive.uiParts.appendHtml(document.querySelector('#baseFooter'), v);
 				} 
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,22 @@
-const path = require('path'); 
+const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   // entry files
-  entry: ['@babel/polyfill', './src/js/main.js', './src/sass/main.scss'],
+  entry: [
+    '@babel/polyfill', 
+    './src/js/ui.global.js', 
+    './src/js/ui.common.js', 
+    './src/sass/ui.base.scss'
+  ],
   // 컴파일 + 번들링된 js 파일이 저장될 경로와 이름 지정
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'js/bundle.js'
+    filename: 'js/ui.base.js'
   },
   plugins: [
     // 컴파일 + 번들링 CSS 파일이 저장될 경로와 이름 지정
-    new MiniCssExtractPlugin({ filename: 'css/style.css' })
+    new MiniCssExtractPlugin({ filename: 'css/ui.base.css' })
   ],
   module: {
     rules: [
